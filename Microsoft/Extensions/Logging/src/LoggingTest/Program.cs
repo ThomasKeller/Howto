c235b843ead5b5b94c36d6d93816d231c94e5dfc
Thomas <mailto.tk@web.de>
2016-09-29 15:27:08 +0200

    Test app for Microsoft.Extension.Logging with Serilog

---
+++ Microsoft/Extensions/Logging/src/LoggingTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+
+namespace LoggingTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Serilog logger
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+
+            // Microsfot Logging Extension
+            var logger = new LoggerFactory()
+                .AddSerilog()
+                .AddConsole()
+                .CreateLogger(typeof(Program).FullName);
+
+            logger.LogInformation("Starting");
+
+            var startTime = DateTimeOffset.UtcNow;
+            logger.LogInformation(1, "Started at {StartTime} and 0x{Hello:X} is hex of 42", startTime, 42);
+
+            try
+            {
+                throw new Exception("Boom");
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical("Unexpected critical error starting application", ex);
+                //logger.Log(LogLevel.Critical, 0, "Unexpected critical error", ex, "");
+                // This write should not log anything
+                //logger.Log<object>(LogLevel.Critical, 0, null, null, null);
+                logger.LogError("Unexpected error", ex);
+                logger.LogWarning("Unexpected warning", ex);
+            }
+
+            using (logger.BeginScope("Main"))
+            {
+                logger.LogInformation("Waiting for user input");
+                var key = Console.Read();
+                logger.LogInformation("User pressed {@KeyInfo}", new { Key = key, KeyChar = (char)key });
+            }
+
+            var endTime = DateTimeOffset.UtcNow;
+            logger.LogInformation(2, "Stopping at {StopTime}", endTime);
+
+            logger.LogInformation("Stopping");
+
+            logger.LogInformation(Environment.NewLine);
+            logger.LogInformation("{Result,-10:l}{StartTime,15:l}{EndTime,15:l}{Duration,15:l}", "RESULT", "START TIME", "END TIME", "DURATION(ms)");
+            logger.LogInformation("{Result,-10:l}{StartTime,15:l}{EndTime,15:l}{Duration,15:l}", "------", "----- ----", "--- ----", "------------");
+            logger.LogInformation("{Result,-10:l}{StartTime,15:mm:s tt}{EndTime,15:mm:s tt}{Duration,15}", "SUCCESS", startTime, endTime, (endTime - startTime).TotalMilliseconds);
+        }
+    }
+}
---
+++ Microsoft/Extensions/Logging/src/LoggingTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggingTest")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb92ab7e-1be4-4579-8f09-79c2ffbc3ab9")]
